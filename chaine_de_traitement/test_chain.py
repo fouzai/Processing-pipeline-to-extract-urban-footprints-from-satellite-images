6bd31d0041639c9e6605fe987cce1a78c80bab52
Adrien André <adrien.andre@seasguyane.org>
2023-04-05 12:25:17 -0300

    Corrections automatiques

--- chaine_de_traitement/test_chain.py
+++ chaine_de_traitement/test_chain.py
@@ -4,30 +4,21 @@
 @author: youssef.fouzai@ird.fr
 """
 from main_chaine_traitement import chaineTraitement
-from file_configuration import s2_file_band, s2_file_clm
 
-
-
-# chemin vers les données en entrée
+# Chemin vers les données d'entrée
 path = "/home/fouzai/chaineTraitement/landsat_donwload"
 
-# chemin vers la region d interet
+# Chemin vers la region d'interêt
 aoi = "/home/fouzai/chaineTraitement/Saint-Laurent-du-Maroni.kmz"
 
-
-# chemin vers le date de sortie
+# Chemin vers la date de sortie
 output_date = "/home/fouzai/chaineTraitement/output.txt"
 
-
-#parametres foto + traitement
-band = [2,3]
+# Paramètres foto + traitement
+band = [2, 3]
 w_size_foto = 11
 w_size_var = 21
-methode_foto = 'block'
+methode_foto = "block"
 threshold = 0.3
-
-
-
-
 
 chaineTraitement(path, aoi, output_date, w_size_foto, w_size_var, methode_foto, threshold, band)
